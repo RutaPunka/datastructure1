839b136eb9490650482d9913ca377948a5f0e36d
Ruta <punkaruta@gmail.com>
2018-11-25 11:23:40 +0200

    Merge branch 'master' of /Users/rutapunka/Desktop/JAVA/datastructure1 with conflicts.

--- src/com.punka.data1/Second.java
+++ src/com.punka.data1/Second.java
@@ -3,4 +3,18 @@ package com.punka.data1;
 public class Second {
     private int number;
 
+    public Second(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int a;
+    int b=6;
 }
